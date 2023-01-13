e00b820f6912825af03f53945874d5816192dd7c
zunobono <zxcrtf245@naver.com>
2023-01-13 14:47:33 +0900

    Create bj2839.py

---
+++ 230113/230113_Class 2++/bj2839.py
@@ -0,0 +1,16 @@
+#설탕 배달
+
+import sys
+
+sugar = int(sys.stdin.readline())
+
+bag = 0
+while sugar >= 0 :
+    if sugar % 5 == 0 :
+        bag += (sugar // 5)
+        print(bag)
+        break
+    sugar -= 3  
+    bag += 1
+else :
+    print(-1)
