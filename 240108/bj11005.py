1de7ad5cd268d02ab7753b37cfcd04704e76ceca
Zunhokim <zxcrtf245@naver.com>
2024-01-08 12:55:58 +0900

    11005번 - 진법변환2

---
+++ 240108/bj11005.py
@@ -0,0 +1,15 @@
+#진법 변환 - 2
+
+N,B = map(int,input().split())
+
+ans = ""
+
+while N!=0:
+    a = N % B
+    if a >= 10:
+        ans += chr(a+55)
+    else:
+        ans += str(a)
+    N //= B
+
+print(ans[::-1])
