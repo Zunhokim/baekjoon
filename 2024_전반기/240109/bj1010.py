26e2a67af8465e42b59517fa92370ce4740fff08
Zunhokim <zxcrtf245@naver.com>
2024-01-09 11:46:24 +0900

    1037번 - 다리 놓기

---
+++ 2024_전반기/240109/bj1010.py
@@ -0,0 +1,14 @@
+#다리 놓기
+#조합을 만들면 될듯
+
+import math
+
+case = int(input())
+combi = []
+
+for i in range(case):
+    n,m = map(int, input().split())
+    combi.append(math.comb(m, n))
+
+for i in combi:
+    print(i)
