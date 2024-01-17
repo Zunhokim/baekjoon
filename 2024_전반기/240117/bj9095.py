6aaf51a114690996c811ddd627cae26a5dd18b51
Zunhokim <zxcrtf245@naver.com>
2024-01-17 16:34:33 +0900

    9095번 - 1,2,3더하기

---
+++ 2024_전반기/240117/bj9095.py
@@ -0,0 +1,17 @@
+#1,2,3 더하기
+
+import sys
+
+T = int(sys.stdin.readline())
+
+arr = [0] * 11
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
+
+for i in range(4, 11):
+    arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+
+for i in range(0, T):
+    case = int(sys.stdin.readline())
+    print(arr[case])
