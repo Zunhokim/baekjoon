e7596f472fc05b112abbddbd4638ef26b37822dc
Zunhokim <zxcrtf245@naver.com>
2024-01-22 14:32:55 +0900

    18870번 - 좌표 압축

---
+++ 2024_전반기/240122/bj18870.py
@@ -0,0 +1,11 @@
+#좌표 압축
+
+import sys
+
+N = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+arr = sorted(set(num))
+dic = {arr[i]:i for i in range(len(arr))}
+
+for i in num:
+    print(dic[i], end=" ")
