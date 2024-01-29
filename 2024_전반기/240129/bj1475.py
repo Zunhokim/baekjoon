ee0e1b0fb323d32849b95bc7d90d810636d48049
Zunhokim <zxcrtf245@naver.com>
2024-01-29 17:02:09 +0900

    1475번 - 방 번호

---
+++ 2024_전반기/240129/bj1475.py
@@ -0,0 +1,18 @@
+#방 번호
+
+import sys
+
+word = sys.stdin.readline().rstrip()
+ans = [0] * 10
+
+for i in range(len(word)):
+    num = int(word[i])
+    if num == 6 or num == 9:
+        if ans[6] <= ans[9]:
+            ans[6] += 1
+        else:
+            ans[9] += 1
+    else:
+        ans[num] += 1
+
+print(max(ans))
